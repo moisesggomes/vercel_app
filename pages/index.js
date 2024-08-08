11e045c20db654df123250759e29db64ee01792c
Moisés Gomes <moisesgomes376@gmail.com>
2024-08-08 11:22:32 -0300

    Adiciona tags Link do Next.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import Link from "next/link"
+
 function Home() {
-    return <h1>Home</h1>
+    return (
+    <div>
+        <h1>Home</h1>
+        <p>
+            <Link href="/sobre">
+                <a>Acessar a página Sobre</a>
+            </Link>
+        </p>
+    </div>
+    )
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+function Sobre() {
+    return (
+    <div>
+        <h1>Sobre</h1>
+        <p>
+            <Link href="/">
+                <a>Acessar a página Home</a>
+            </Link>
+        </p>
+    </div>
+    )
+}
+
+export default Sobre
